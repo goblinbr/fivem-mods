84350489c3e60317b3112cb788193bd94eaa7f9a
Rodrigo de Bona Sartor <rodrigo.goblin@gmail.com>
2022-09-24 19:44:45 -0300

    Fix on newer builds
    https://forum.cfx.re/t/b2060-b2189-game-event-ceventnetworkentitydamage-not-working-as-expected/1922652/8?u=goblinbr

--- npc-kill/client/main.lua
+++ npc-kill/client/main.lua
@@ -1,8 +1,18 @@
+local BUILD = GetGameBuildNumber()
+
 AddEventHandler('gameEventTriggered',function(name,args)
     if name == 'CEventNetworkEntityDamage' then
         local victimId = args[1]
         local attackerId = args[2]
         local isFatalDamage = args[4]
+
+        if BUILD >= 2189 then
+            isFatalDamage = args[6]
+        else
+            if BUILD >= 2060 then
+                isFatalDamage = args[5]
+            end
+        end
 
         if isFatalDamage == 1 then
             local pedId = PlayerPedId()
@@ -22,17 +32,3 @@ AddEventHandler('gameEventTriggered',function(name,args)
         print('CLIENT game event ' .. name .. ' (' .. json.encode(args) .. ')')
     end
 end)
-
--- [182] - CEventNetworkEntityDamage
--- IDX  Type	Returned Value
--- 1    integer	Victim (Entity ID)
--- 2    integer	Attacker (Entity ID)
--- 3    integer	1101169152
--- 4    boolean	Damage fatal
--- 5    hash	WeaponInfoHash, for example; 133987706 is joaat("WEAPON_RAMMED_BY_CAR")) - ref: common.rpf/data/ai/weapons.meta->Item type="CWeaponInfo"
--- 6    integer	1086820063
--- 7    integer	1086820063
--- 8    unknown	0
--- 9    unknown	0
--- 10   boolean	Is Melee damage
--- 11   integer	Unknown, but 0 for peds, 116 for the body of a vehicle, 93 for a tire, 120 for a side window, 121 for a rear window, 122 for a windscreen, etc
